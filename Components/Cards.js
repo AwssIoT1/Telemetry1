0da53c38bf0872d7f9ea9331ce6e4572c74d1c45
Sam <Vintechnologies.io@outlook.com>
2022-01-06 17:21:20 +0530

    usingPusherJs

--- Components/Cards.js
+++ Components/Cards.js
@@ -4,31 +4,39 @@ import Pusher from 'pusher-js';
 import Card from "./Card.js"
 import '../css/cards.css';
 // import { Divider } from "semantic-ui-react";
-//import usePusher, {data1} from '../Providers/pusher/UsePusher.js';
+import usePusher,{ Power_MW} from '../Providers/pusher/UsePusher.js';
 // ------------------------------------
-Pusher.logToConsole = true;
-const pusher = new Pusher(
-  "768a2152b7c6fd195a2f", 
-  {
-    cluster: "ap2", 
-    forceTLS: true,
-  }
+// Pusher.logToConsole = true;
+// const pusher = new Pusher(
+//   "768a2152b7c6fd195a2f", 
+//   {
+//     cluster: "ap2", 
+//     forceTLS: true,
+//   }
  
-);
-const channel = pusher.subscribe("chData"); 
-
+// );
+// const channel = pusher.subscribe("chData"); 
+// const channel2 = pusher.subscribe("chData_1h10m"); 
 //-----------------------
-var count = 0;
-const Cards = ({SignalName}) => {
-    const sig1 = SignalName
-    var data121;   
-    const dd ="34"
-   
-    const [Power_MW, setPower_MW] = useState(() =>{return 0});
-    const [Power_MVAR, setPower_MVAR] = useState(() =>{return 0});
-    const [Frequency_Hz, setFrequency_Hz] = useState(() =>{return 0});
-    const [PowerFactor, setPowerFactor] = useState(() =>{return 0});
-    const [Time, setTime] = useState(() =>{return 0});
+
+
+
+
+const Cards = () => {
+  const {Power_MW, Power_MVAR, Frequency_Hz, PowerFactor,Time,count} = usePusher();
+  console.log({count})
+  // const Power_MW = 6
+  // const Power_MVAR = 6
+  //   const Frequency_Hz = 7
+  //   const PowerFactor = 9
+  //   const Time = 10
+  
+    // const [Power_MW, setPower_MW] = useState(() =>{return 0});
+    // const [Power_MVAR, setPower_MVAR] = useState(() =>{return 0});
+    // const [Frequency_Hz, setFrequency_Hz] = useState(() =>{return 0});
+    // const [PowerFactor, setPowerFactor] = useState(() =>{return 0});
+    // const [Time, setTime] = useState(() =>{return 0});
+    // const [Data_1h10m, setData_1h10m] = useState([]);
    
     var DateNow =    new Date();
     
@@ -48,27 +56,33 @@ const Cards = ({SignalName}) => {
     
     var myDate = new Date(1000*Time);
     // --------------------------------------------------------------------------------
-   
-
-
-        count = count + 1;
-        console.log(count)
-
-        
+       
   
               //parse data
-    const getval = (data) =>{
-      var vals = JSON.parse(data.payload)
-      setPower_MW(vals.Power_MW)
-      setPower_MVAR(vals.Power_MVAR)
-      setFrequency_Hz(vals.Frequency_Hz)
-      setPowerFactor(vals.PowerFactor)
-      setTime(vals.Time)
-    }
+    // const getval = (data) =>{
+    //   testdata = data
+    //   var vals = JSON.parse(data.payload)
+    //   setPower_MW(vals.Power_MW)
+    //   setPower_MVAR(vals.Power_MVAR)
+    //   setFrequency_Hz(vals.Frequency_Hz)
+    //   setPowerFactor(vals.PowerFactor)
+    //   setTime(vals.Time)
+    // }
   
   
-    channel.bind("Power",getval);       
-         
+ 
+    //     const getval2 = (data) =>{
+    //       var vals2 = JSON.parse(data.payload)
+    //       console.log(vals2.Data_1h10m.val0.Frequency)
+    //       }
+    
+      
+      // setData_1h10m(vals2.Data_1h10m)
+      // console.log(Data_1h10m)
+    
+  
+    // channel.bind("Power",getval);       
+    // channel2.bind("Power",getval2);      
     // --------------------------------------------------------------------------------
 
 return (
--- Providers/pusher/UsePusher.js
+++ Providers/pusher/UsePusher.js
@@ -1,17 +1,17 @@
 
- import React, { Component } from 'react';
+ import { useState, useEffect } from 'react';
  import Pusher from 'pusher-js';
 
 // Default
 
-export const SUCCESS = 'SUCCESS';
-export const FAILURE = 'FAILURE';
+// export const SUCCESS = 'SUCCESS';
+// export const FAILURE = 'FAILURE';
 
-// Initial state
-const initialState = {
-    data: null,
-    errorMessage: null,
-  };
+// // Initial state
+// const initialState = {
+//     data: null,
+//     errorMessage: null,
+//   };
 
   Pusher.logToConsole = true;
   const pusher = new Pusher(
@@ -21,18 +21,55 @@ const initialState = {
       forceTLS: true,
     }
   );
-  //fetch data
-  const channel = pusher.subscribe("chData");
-  const usePusher = () => {
 
-    channel.bind("Power", (data1) => 
-    {
-    
-         console.log(data1)
-      
-        // return {data1}
-    });
+  //---------------------------------------------------------
+  const channel = pusher.subscribe("chData");
+  const channel2 = pusher.subscribe("chData_1h10m"); 
+   //---------------------------------------------------------
    
+  
+  const usePusher = () => {
+     const [Power_MW, setPower_MW] = useState(() =>{return 0});
+      const [Power_MVAR, setPower_MVAR] = useState(() =>{return 0});
+    const [Frequency_Hz, setFrequency_Hz] = useState(() =>{return 0});
+    const [PowerFactor, setPowerFactor] = useState(() =>{return 0});
+    const [Time, setTime] = useState(() =>{return 0});
+    const [Data_1h10m, setData_1h10m] = useState([0]);
+    const [count, setcount] = useState([0]);
+    //UseState
+    
+   
+   //----------------------------------------------------------
+
+  
+    useEffect(() => {
+
+      const getval = (data) =>{
+       
+        var vals = JSON.parse(data.payload)
+        
+        setPower_MW(vals.Power_MW)
+        setPower_MVAR(vals.Power_MVAR)
+        setFrequency_Hz(vals.Frequency_Hz)
+        setPowerFactor(vals.PowerFactor)
+        setTime(vals.Time)
+       
+    }
+    channel.bind("Power",getval);  
+    //-----------------------------------------
+        const getval2 = (data) =>{
+            
+         var  vals2 = JSON.parse(data.payload)
+                 console.log(vals2.val0, vals2.val1)
+                setcount(vals2)
+          }
+          channel2.bind("Power",getval2);
+        },[]);
+
+         
+    
+    
+    return  {Power_MW, Power_MVAR, Frequency_Hz, PowerFactor,Time,count} ;
   
     };
    
